07dedb72c43a6d4503fe36adec4ffdefda281731
Zhiming Guo <gzmkobe@gmail.com>
2016-07-29 04:24:33 +0800

    Third

--- server.R
+++ server.R
@@ -1,17 +1,13 @@
 library(shiny)
-<<<<<<< HEAD
 library(devtools)
 library(ggplot2)
 library(hurricaneexposuredata)
-=======
-library(ggplot2)
->>>>>>> 7aec4718e9af1bd1455ca320f2a068ca2ae22e74
 library(hurricaneexposure)
 
 
 
 shinyServer(function(input, output) {
-<<<<<<< HEAD
+
   
   output$map <-renderPlot({
     a <- map_counties(storm = input$storm_id, metric = input$metric)
@@ -20,12 +16,5 @@ shinyServer(function(input, output) {
                      
     })
   
-=======
 
-  output$mapPlot <- renderPlot({
-    a <- map_counties(storm = input$storm_id, metric = input$metric)
-    map_tracks(storms = input$storm_id, plot_object = a, plot_points = FALSE) + 
-      ggtitle(paste(input$storm_id, input$metric, sep = ", "))
-  })
->>>>>>> 7aec4718e9af1bd1455ca320f2a068ca2ae22e74
 })
--- ui.R
+++ ui.R
@@ -1,5 +1,4 @@
 library(shiny)
-<<<<<<< HEAD
 library(hurricaneexposure)
 library(hurricaneexposuredata)
 data("hurr_tracks")
@@ -8,12 +7,12 @@ storms <- unique(hurr_tracks$storm_id)
 storm_years <- as.numeric(gsub(".+-", "", storms))
 storms <- storms[storm_years <= 2011]
 
-=======
+
 data("hurr_tracks")
 storms <- unique(hurr_tracks$storm_id)
 storm_years <- as.numeric(gsub(".+-", "", storms))
 storms <- storms[storm_years <= 2011]
->>>>>>> 7aec4718e9af1bd1455ca320f2a068ca2ae22e74
+
 
 shinyUI(fluidPage(
   
@@ -22,7 +21,7 @@ shinyUI(fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-<<<<<<< HEAD
+
       
       selectInput("storm_id",label="storm_id",
                   storms
@@ -31,21 +30,8 @@ shinyUI(fluidPage(
       selectInput("metric", label="Storm Exposure Metric:",
                  choices =  c("distance","rainfall","wind"),
                  selected = "distance")
-=======
-      # selectInput("year",label = "year",
-      #             c("1999"="1999")
-      # 
-      # ),
 
-      selectInput("storm_id",label="storm_id",
-                  storms
-      ),
-
-      selectInput("metric", label="Storm exposure metric:",
-                  c("distance" = "distance",
-                    "rainfall" = "rainfall",
-                    "wind" = "wind"))
->>>>>>> 7aec4718e9af1bd1455ca320f2a068ca2ae22e74
+    
                  ),
     
     mainPanel(plotOutput("map")))
