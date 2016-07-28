d1cfb7c21ed5621446465b6a925b88f25041df17
Zhiming Guo <gzmkobe@gmail.com>
2016-07-29 07:39:30 +0800

    add some comments

--- server.R
+++ server.R
@@ -11,7 +11,8 @@ shinyServer(function(input, output) {
   
   output$map <-renderPlot({
     a <- map_counties(storm = input$storm_id, metric = input$metric)
-    map_tracks(storms = input$storm_id, plot_object = a, plot_points = FALSE) + 
+    map_tracks(storms = input$storm_id, plot_object = a, plot_points = FALSE) + ### This code runs slower because if we dont change storm_id, only change
+     ### Metric, the map_tracks function wont be changed. Indeed, it re-runs map_tracks() so it takes long time to output.
       ggtitle(paste(input$storm_id, input$metric, sep = ", "))
                      
     })
