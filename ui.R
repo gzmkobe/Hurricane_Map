9deda30efa6b0c696f6bb77a688c595b7c1c88ae
Zhiming Guo <gzmkobe@gmail.com>
2016-09-10 00:31:49 -0600

    Desrciption is enriched

--- ui.R
+++ ui.R
@@ -26,6 +26,10 @@ shinyUI(dashboardPage(skin = "blue",
                             centered on the storm date. This work was supported in part by grants from
                             the National Institute of Environmental Health Sciences (R00ES022631) and
                             the National Science Foundation (1331399).", color = "black", width = 400),
+                   valueBox("User Manual", "Once you select a year, for example, 1998. Then you need to specify the storm name which 
+                            was occured in that year. After that, you need to specify the desired exposure metric. 
+                            After all the inputs have been specified, the corresponding map(s) and table would be created on the right hand 
+                            side. Also, you are allowed to download the table if data is available in it", color = "black", width = 400),
                    width = 400,
                    "For more information, please click the link below",
                    menuItem("Hurricane Exposure", icon = icon("fa fa-github"), 
